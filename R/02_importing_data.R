7ba0cb9590c5557af8d938cb67ee637d666c1852
biavarejao <73177791+biavarejao@users.noreply.github.com>
2022-08-10 11:36:58 -0300

    using graphics in R

--- R/02_importing_data.R
+++ R/02_importing_data.R
@@ -22,7 +22,7 @@ length(data_list)
 # Inspecting data
 head(data_list$envir)
 dim(data_list$envir)
-summary(envir$envir)
+summary(dahelpta_list$envir)
 
 # Output 1: summary table ------------------------------------------------------
 
